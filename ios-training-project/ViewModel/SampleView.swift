c879c66ae8a309bd6195161eeda0476bb2f11fc3
Harshad Sawant <harshads2022@gmail.com>
2024-11-25 13:29:35 +0530

    mvvm folder created

---
+++ ios-training-project/ViewModel/SampleView.swift
@@ -0,0 +1,18 @@
+//
+//  SampleView.swift
+//  ios-training-project
+//
+//  Created by Harshad Sawant on 25/11/24.
+//
+
+import SwiftUI
+
+struct SampleView: View {
+    var body: some View {
+        Text("Sample View!")
+    }
+}
+
+#Preview {
+    SampleView()
+}
