ff0e3439477582037275fe4e3faf38a264394ffa
Harshad Sawant <harshads2022@gmail.com>
2024-11-25 13:03:20 +0530

    Initial Commit

---
+++ ios-training-project/ios_training_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_training_projectApp.swift
+//  ios-training-project
+//
+//  Created by Harshad Sawant on 25/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_training_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
